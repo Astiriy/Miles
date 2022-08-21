cd622a264e3fbfea6e6a8053c0cb80b017707b9d
Ореолов Евгений <evoreo@yandex.ru>
2022-08-22 01:28:30 +0300

    new file:   .gitignore 	new file:   .idea/.gitignore 	new file:   .idea/misc.xml 	new file:   .idea/modules.xml 	new file:   .idea/vcs.xml 	new file:   miles.iml 	new file:   src/Main.java

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+       int x = 51764; // Цена билета
+       int y = 20;
+       int z = x / y;
+        System.out.println("Количество миль: " + z);
+
+    }
+}
