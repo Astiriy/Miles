600a234009ec1b20c30fab450f4c8789a90f9f9f
Ореолов Евгений <evoreo@yandex.ru>
2022-08-22 01:30:45 +0300

    modified:   src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-       int x = 51764; // Цена билета
-       int y = 20;
-       int z = x / y;
+        int x = 51764; // Цена билета
+        int y = 20;
+        int z = x / y;
         System.out.println("Количество миль: " + z);
 
     }
