180271db34e3d75deee7bd952fa8e396d609e732
Astiriy <107921897+Astiriy@users.noreply.github.com>
2022-08-30 21:20:45 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        int x = 51764; // Цена билета
-        int y = 20;
-        int z = x / y;
-        System.out.println("Количество миль: " + z);
+        int price = 51764; // Цена билета
+        int cost = 20;
+        int miles = x / y;
+        System.out.println("Количество миль: " + miles);
 
     }
 }
